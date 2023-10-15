a266f243b7b7ad2c4b98b3c781aa4e241a9cad7b
WAKenney <80290200+WAKenney@users.noreply.github.com>
2023-10-15 20:52:28 +0000

    updated some wording

--- 1_Home.py
+++ 1_Home.py
@@ -11,10 +11,10 @@ st.markdown("""
         Using NWAnalytics, you can map and analyze various aspects of the urban forest that will help you develop and implement stewardship strategies.
         At present, you must first have your Neighbourwoods tree inventory data in a Neighbourwoods MS excel workbook (version 2.6 or greater).
 
-        To get started, select your Neighbourwoods MS excel workbook at the sidebar on the left. Once your data has been uploaded (this may take 
-        a few minutes if you have a big file, be patient) you will be asked to select the functions you want to display.  Select as many as you 
-        want from the dropdown list __AND CLICK ON CONTINUE__.  The selected analyses will be shown in the main frame.
-
+        To get started, select the section called "Create or Refresh Summary Workbook" from the sidebar at the left. Select or drag and drop
+        your Neighbourwoods data excel workbook into the box shown. Once your data has been uploaded (this may take 
+        a few minutes if you have a big file, be patient) you can go the appropriate sections to the perform the various tasks.
+            
         You can conduct these analyses on all the data, or you can filter the data for specific queries. For hints on filtering your data, click on the button below.
 
         In various places you will have opportunities to click on a box 
