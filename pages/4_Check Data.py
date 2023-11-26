e7c35a44e7169a0f45af15872fbb0ab3ca867ff1
WAKenney <80290200+WAKenney@users.noreply.github.com>
2023-11-26 15:52:28 +0000

    fixed various bugs from transfer

--- pages/4_Check Data.py
+++ pages/4_Check Data.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 #Create page title
 titleCol1, titleCol2, titleCol3 =st.columns((1,4,1))
@@ -11,31 +12,36 @@ st.subheader('Create or Refresh a Neighbourwoods Summary File')
 
 st.markdown("___")
 
-st.write(st.session_state['df_trees'].columns)
-
 df_trees = st.session_state['df_trees']
 
-# df_trees.rename(columns = {'Tree Name':'tree_name','Date':'date','Block Id':'block','Block ID':'block','Tree No':'tree_number',
-#                                 'House Number':'house_number',
-#                                 'location':'location_code','Location Code':'location_code',
-#                                 'ownership':'ownership_code','Ownership code':'ownership_code', 'Ownership Code':'ownership_code',
-#                                 'Crown Width':'crown_width','Number of Stems':'number_of_stems','DBH':'dbh',
-#                                 'Hard surface':'hard_surface','Hard Surface':'hard_surface','Ht to base':'height_to_crown_base',
-#                                 'Total Height':'total_height','Reduced Crown':'reduced_crown','Unbalanced Crown':'unbalanced_crown',
-#                                 'Defoliation':'defoliation','Weak or Yellow Foliage':'weak_or_yellow_foliage','Weak or Yellowing Foliage':'weak_or_yellow_foliage',
-#                                 'Dead or Broken Branch':'dead_or_broken_branch','Lean':'lean','Poor Branch Attachment':'poor_branch_attachment',
-#                                 'Branch Scars':'branch_scars','Trunk Scars':'trunk_scars','Conks':'conks','Rot or Cavity - Branch':'branch_rot_or_cavity',
-#                                 'Rot or Cavity - Trunk':'trunk_rot_or_cavity','Confined Space':'confined_space',
-#                                 'Crack':'crack','Girdling Roots':'girdling_roots', 'Exposed Roots': 'exposed_roots', 'Recent Trenching':'recent_trenching',
-#                                 'Cable or Brace':'cable_or_brace','Conflict with Wires':'wire_conflict',
-#                                 'Conflict with Sidewalk':'sidewalk_conflict','Conflict with Structure':'structure_conflict',
-#                                 'Conflict with another tree':'tree_conflict','Conflict with Traffic Sign':'sign_conflict',
-#                                 'genus':'Genus', 'family':'Family', 'cpa':'Crown Projection', 'rdbh':'Relative DBH', 
-#                                 'rdbh_class':'Relative DBH Class', 'dbh_class':'DBH Class', 'native':'Native', 
-#                                 'color':'Color', 'health':'Health Defects', 'structural':'Structural Defects', 
-#                                 'diversity_level':'Diversity Level', 'invasivity':'Invasivity', 
-#                                 'comments':'Comments', 'suitability':'Species Suitability', 
-#                                 'seRegion':'Site Region', 'description':'Description', 'geometry':'Geometry'}, inplace = True)
+df_trees.rename(columns = {'Tree Name' : 'tree_name', 'Date' : 'date', 'Block ID' : 'block', 'Block Id':'block',
+                            'Tree Number' : 'tree_number', 'House Number' : 'house_number', 'Street Code' : 'street_code', 
+                            'Species Code' : 'species_code', 'Location Code' : 'location_code', 'location':'location_code', 
+                            'Ownership Code' : 'ownership_code','ownership':'ownership_code','Ownership code':'ownership_code', 
+                            'Number of Stems' : 'number_of_stems', 'DBH' : 'dbh', 'Hard Surface' : 'hard_surface', 
+                            'Crown Width' : 'crown_width', 'Ht to Crown Base' : 'height_to_crown_base', 
+                            'Total Height' : 'total_height', 'Reduced Crown' : 'reduced_crown', 
+                            'Unbalanced Crown' : 'unbalanced_crown', 'Defoliation' : 'defoliation', 
+                            'Weak or Yellowing Foliage' : 'weak_or_yellow_foliage', 
+                            'Dead or Broken Branch' : 'dead_or_broken_branch', 'Lean' : 'lean', 
+                            'Poor Branch Attachment' : 'poor_branch_attachment', 'Branch Scars' : 'branch_scars', 
+                            'Trunk Scars' : 'trunk_scars', 'Conks' : 'conks', 'Rot or Cavity - Branch' : 'branch_rot_or_cavity', 
+                            'Rot or Cavity - Trunk' : 'trunk_rot_or_cavity', 'Confined Space' : 'confined_space', 
+                            'Crack' : 'crack', 'Girdling Roots' : 'girdling_roots', 'Exposed Roots' :  'exposed_roots', 
+                            'Recent Trenching' : 'recent_trenching', 'Cable or Brace' : 'cable_or_brace', 
+                            'Conflict with Wires' : 'wire_conflict', 'Conflict with Sidewalk' : 'sidewalk_conflict', 
+                            'Conflict with Structure' : 'structure_conflict', 'Conflict with Another Tree' : 'tree_conflict', 
+                            'Conflict with Traffic Sign' : 'sign_conflict', 'Comments' : 'comments', 
+                            'Longitude' : 'longitude', 'Latitude' : 'latitude', 
+                            'Street' : 'street', 'Family' : 'family', 'Genus' : 'genus', 'Species' : 'species', 
+                            'Invasivity' : 'invasivity', 'Species Suitability' : 'suitability', 
+                            'Diversity Level' : 'diversity_level', 'Native' : 'native', 'Crown Projection Area (CPA)' : 'cpa', 
+                            'Address' : 'address', 'DBH Class' : 'dbh_class', 'Relative DBH' : 'rdbh', 'Relative Dbh' : 'rdbh', 
+                            'Relative DBH Class' : 'rdbh_class', 'Structural Defects' : 'structural', 
+                            'Health Defects' : 'health', 'Description' : 'description', 'Defects' : 'defects', 
+                            'Defect Colour' : 'defectColour',  'Total Demerits' : 'demerits', 'Simple Rating' : 'simple_rating'},
+                            inplace = True)
+
 
 def check_data(df):
     '''Checks df_trees for anomolies and adds a column showing errors that may crash the program
@@ -46,11 +52,9 @@ def check_data(df):
         # Older versions of the data didn't include surface roots.  To avoid crashing the app, this function checks to see if surface roots are in the data.
         # If not, then this attribute is removed from the list of column names (cols)
 
-        # df = chkData.copy()
-
         if df['exposed_roots'].isnull().all():
 
-            cols = ['tree_name','latitude', 'longitude', 'block', 'species', 'street', 'address', 'location_code', 'ownership_code', 'number_of_stems', 'dbh',
+            cols = ['tree_name','latitude', 'longitude', 'block', 'species', 'street_name', 'address', 'location_code', 'ownership_code', 'number_of_stems', 'dbh',
             'hard_surface', 'crown_width', 'height_to_crown_base', 'total_height', 'reduced_crown', 'unbalanced_crown', 'defoliation',
             'weak_or_yellow_foliage', 'dead_or_broken_branch', 'lean', 'poor_branch_attachment', 'branch_scars', 'trunk_scars', 'conks',
             'branch_rot_or_cavity', 'trunk_rot_or_cavity', 'confined_space','crack', 'girdling_roots', 'recent_trenching', 'cable_or_brace',
@@ -59,11 +63,13 @@ def check_data(df):
             st.write('It appears that you did not record exposed roots for any entries.  This attribute will be ignored in the "check data" function.')
 
         else:
-            cols = ['tree_name','latitude', 'longitude', 'block', 'species', 'street', 'address', 'location_code', 'ownership_code', 'number_of_stems', 'dbh',
+            cols = ['tree_name','latitude', 'longitude', 'block', 'species', 'street_name', 'address', 'location_code', 'ownership_code', 'number_of_stems', 'dbh',
             'hard_surface', 'crown_width', 'height_to_crown_base', 'total_height', 'reduced_crown', 'unbalanced_crown', 'defoliation',
             'weak_or_yellow_foliage', 'dead_or_broken_branch', 'lean', 'poor_branch_attachment', 'branch_scars', 'trunk_scars', 'conks',
             'branch_rot_or_cavity', 'trunk_rot_or_cavity', 'confined_space','crack', 'girdling_roots', 'exposed_roots', 'recent_trenching', 'cable_or_brace',
             'wire_conflict', 'sidewalk_conflict', 'structure_conflict', 'tree_conflict', 'sign_conflict']
+
+        
 
         # Conks is the only attibute with values of 0 or 1 so a species list of column names EXCLUDING conks is set up called 'conditionColsNoConks'
 
@@ -76,10 +82,16 @@ def check_data(df):
         trees = df['tree_name']
        
        #don't need these columnns since they are derived values
-        df.drop(['genus', 'family','cpa', 'rdbh', 'rdbh_class', 'dbh_class', 'native', 'color', 'health','structural', 'defects', 'defectColour', 'diversity_level', 'invasivity',
-            'comments', 'suitability', 'seRegion', 'description', 'geometry'], 
-            axis=1, inplace=True) 
+        # df.drop(['genus', 'family','cpa', 'rdbh', 'rdbh_class', 'dbh_class', 'health','structural', 'defects', 'defectColour', 'diversity_level', 'invasivity',
+        #     'comments', 'suitability', 'description'], 
+        #     axis=1, inplace=True)   
+      
+        # df.drop(['genus', 'family','cpa', 'rdbh', 'rdbh_class', 'dbh_class', 'native', 'color', 'health','structural', 'defects', 'defectColour', 'diversity_level', 'invasivity',
+        #     'comments', 'suitability', 'seRegion', 'description', 'geometry'], 
+        #     axis=1, inplace=True) 
         
+
+
         # Demerits and simple_rating are columns in the legacy format of Neighbourwoods MS 2.6.  If they are present they will be dropped
         if "demerits" in df.columns:
             df.drop(['demerits'])
@@ -221,10 +233,11 @@ def check_data(df):
                         You can filter the columns just as you would in the main table.
         ''')
 
-        # st.dataframe(dfCheck)
+        st.dataframe(dfCheck, hide_index=True)
 
 if "df_trees" not in st.session_state:
 
     st.error("You haven't loaded a file yet.  Either go to the 'Create or Refresh...' function in the side bar or the ' Load and Existing...")
 
-check_data(df_trees)
+
+check_data(st.session_state['df_trees'])
